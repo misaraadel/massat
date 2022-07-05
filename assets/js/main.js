f2ccc09a505b5f4c8b28dec02139572b1f1c2a4e
misaraadel <41232116+misaraadel@users.noreply.github.com>
2022-07-05 23:21:46 +0200

    add landing page

---
+++ assets/js/main.js
@@ -0,0 +1,82 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 700) {
+            $(".scroll-top-button").fadeIn();
+        } else {
+            $(".scroll-top-button").fadeOut();
+        }
+    });
+
+    $(function () {
+        $(document).scroll(function () {
+            var $nav = $(".navbar");
+            $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
+    });
+
+    $(".scroll-top-button").on('click', function () {
+        $('html , body').animate({
+            scrollTop: 0
+        }, 100);
+    });
+
+    $('.hamburger').click(function(){
+        $('.hamburger').toggleClass('active');
+        $('.nav-contain').toggleClass('active-nav');
+        $('body').toggleClass('overflowNone')
+    });
+
+    var swiper = new Swiper('.team-swiper', {
+        loop: true,
+        speed: 900,
+        spaceBetween: 15,
+        autoplay: {
+            delay: 2500,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            300: {
+                slidesPerView: 1,
+            },
+            640: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 4,
+            },
+        }
+    });
+
+    var swiper = new Swiper('.partners-swiper', {
+        loop: true,
+        speed: 900,
+        spaceBetween: 5,
+        autoplay: {
+            delay: 2500,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            300: {
+                slidesPerView: 2,
+            },
+            640: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 6,
+            },
+        }
+    });
+});
